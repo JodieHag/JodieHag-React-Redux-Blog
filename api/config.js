8afca0b74a905241ccef110e20934e03d6b69aa0
JodieHag <info@jodiehag.com>
2017-07-30 02:55:45 +0200

    test monog

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://info@jodiehag.com:Mcgrady13@fierce-bayou-34347/blog'
+  db: process.env.MONGODB || 'mongodb://JodieHag:Mcgrady13@fierce-bayou-34347/blog'
 };
