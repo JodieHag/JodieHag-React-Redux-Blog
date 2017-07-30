3b627d54e66a59748fac07a37792c139f17f4d88
JodieHag <info@jodiehag.com>
2017-07-30 03:04:35 +0200

    test monog

--- api/app.js
+++ api/app.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const api = require('./routes');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.get(['/', '/blog', '/cuentamecosas'], (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
